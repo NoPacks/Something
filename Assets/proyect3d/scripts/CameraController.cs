6b800a594863e5112e5dbcce3a699b9de9051a48
Sanca Venegas, Martin Joel <splayer003@gmail.com>
2019-05-03 20:39:41 -0500

    Initial commit

---
+++ Assets/proyect3d/scripts/CameraController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("Camera")]
+    public float CameraSpeed;
+    private float ejeX, ejeY;
+    public float ClampAngle;
+    public Transform Tarjet, playerPivot;
+
+    [Header("LineCast")]
+    public float Max;
+    public float Min;
+    private Vector3 Dir;
+
+    [Header("Adjust")]
+    public float smoth;
+    public float Distance;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Dir = transform.localPosition.normalized;
+        Distance = transform.localPosition.magnitude;
+    }
+
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        Vector3 DesiredCameraPos = transform.parent.TransformPoint(Dir * Max);
+        RaycastHit hit;
+
+        
+        if (Physics.Linecast(transform.parent.position, DesiredCameraPos, out hit))
+        {
+            Distance = Mathf.Clamp((hit.distance * 0.8f), Min, Max);
+        }
+        else
+        {
+            Distance = Max;
+        }
+
+        transform.localPosition = Vector3.Lerp(transform.localPosition, Dir * Distance, Time.deltaTime * smoth);
+    }
+
+    void LateUpdate()
+    {
+        CameraMovement();
+    }
+
+    private void CameraMovement()
+    {
+        ejeX += Input.GetAxis("Mouse X") * CameraSpeed;
+        ejeY -= Input.GetAxis("Mouse Y") * CameraSpeed;
+
+        ejeY = Mathf.Clamp(ejeY, -ClampAngle, ClampAngle);
+
+        playerPivot.rotation = Quaternion.Euler(0, ejeX, 0);
+        Tarjet.rotation = Quaternion.Euler(ejeY, ejeX, 0);
+
+    }
+}
---
+++ Assets/proyect3d/scripts/PlayerInput.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class PlayerInput : MonoBehaviour
+{
+    Animator anim;
+    Rigidbody rb;
+    float inputX;
+    float inputZ;
+    public float Speed;
+    public float jumpForce;
+    public int jumpCounter;
+    public bool isGrounded;
+    public GameObject playerPivot;
+    public CinemachineVirtualCamera myVirtualCamera;
+    private CinemachineImpulseSource myImpulseSource;
+
+    public Transform attackSpawner;
+
+    [Header("Instance")]
+    public GameObject hitBox;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        myImpulseSource = myVirtualCamera.GetComponent<CinemachineImpulseSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputX = Input.GetAxis("Horizontal");
+        inputZ = Input.GetAxis("Vertical");
+        anim.SetFloat("MovX", inputX);
+        anim.SetFloat("MovZ", inputZ);
+
+        
+
+        if (inputX == 0 && inputZ == 0)
+        {
+            anim.SetBool("Static", true);
+        } else
+        {
+            anim.SetBool("Static", false);
+            transform.rotation = Quaternion.Lerp(transform.rotation, playerPivot.transform.rotation, Speed * Time.deltaTime);
+        }
+
+        Vector3 movement = new Vector3(inputX * Speed, rb.velocity.y,inputZ * Speed);
+        movement = playerPivot.transform.rotation * movement;
+
+        rb.velocity = movement;
+        
+
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCounter< 1)
+        {
+            anim.SetTrigger("Jumping");
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+            isGrounded = false;
+            anim.SetBool("isGrounded", false);
+            jumpCounter++;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, playerPivot.transform.rotation, Speed * Time.deltaTime);
+            anim.SetTrigger("Slash");
+        }
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Floor")
+        {
+            jumpCounter = 0;
+            isGrounded = true;
+            anim.SetBool("isGrounded", true);
+        }
+    }
+
+    private void Attack()
+    {
+        myImpulseSource.GenerateImpulse(Vector3.right);
+        Instantiate(hitBox, attackSpawner.position, transform.rotation);
+        Debug.Log("shake");
+    }
+}
